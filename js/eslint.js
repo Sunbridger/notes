44a493361898df87398a6983d94a5acb789c2691
Sunbridger <739272884@qq.com>
2019-07-13 15:20:51 +0800

    eslint 规范

---
+++ js/eslint.js
@@ -0,0 +1,18 @@
+const a = 3;
+const obj = {
+    say(x, y) {
+        console.log(x, y, 'aaaa');
+    },
+};
+
+const playa = { name: 'sun', age: 2, pl: 'asdasd' };
+const op = {
+    age: 3,
+    fn() {
+        console.log(22);
+    },
+    arro: () => {
+        console.log(7777);
+    },
+};
+obj.say(1, 2);
