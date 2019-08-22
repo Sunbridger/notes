cd5bb01a94514ba31d51fb635f2627ecd8b8fe49
Sunbridger <739272884@qq.com>
2019-08-22 22:37:23 +0800

    slice

---
+++ js/slice.js
@@ -0,0 +1,10 @@
+let str = 'abcdefg';
+let arr = [1, 2, 3];
+let s = str.slice(1, 3);
+let a = arr.slice(1, 3)
+console.log(s, str, a, arr);
+/**
+ * bc abcdefg [ 2, 3 ] [ 1, 2, 3 ]
+ * slice(start: number, end: number) 数组 字符串均可
+ */
+
