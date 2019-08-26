7e921cb8e01bcc3f20f797b750a0bcec2bc166e6
Sunbridger <739272884@qq.com>
2019-08-26 14:51:36 +0800

    feat: 修改

--- js/babel2.js
+++ js/babel2.js
@@ -2,13 +2,30 @@ const code = `
 import Vue from 'vue';
 import vueLoader from 'vue-loader';
 Vue.use(vueLoader);
+`;
+
+
+let p = require("@babel/parser");
+// let cp = p.parse(code, {
+//     sourceType: "module",
+//     plugins: [
+//         "jsx"
+//     ]
+// });
+let code2 = `
+    let a = 'code2';
+    console.log(a, '---a');
+    function asy() {
+        let v = await 9;
+        console.log(v, '--v');
+    }
+    say();
 `
-let cp = require("@babel/parser").parse(code, {
-    sourceType: "module",
-    plugins: [
-        "jsx"
-    ]
+let cp2 = p.parse(code2, {
+    allowReturnOutsideFunction: true
 });
-require('fs').writeFile('./js/data2.json',JSON.stringify(cp),(err)=>{
-    if(err)console.log(err);
-})
+
+console.log(cp, '----', cp2);
+// require('fs').writeFile('./js/data2.json',JSON.stringify(cp),(err)=>{
+//     if(err)console.log(err);
+// })
