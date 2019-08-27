b10587411ce8300709e7d2a0b9933b57b4efacbe
Sunbridger <739272884@qq.com>
2019-08-27 16:27:05 +0800

    feat: 修改

--- js/mapa.js
+++ js/mapa.js
@@ -5,11 +5,13 @@ let arr =[{
     e: 2,
     c: 3
 }];
-let a = arr;
+// let a = arr;
+//  let a = Object.assign([], arr);
+let a = JSON.parse(JSON.stringify(arr));
 a = a.map((el) => {
     if (el.e === 1) {
         delete el.e
     }
     return el;
 })
-console.log(a, arr)
+console.log(arr)
