596cbdfefcb84f081940273c68aa443213f693ba
Sunbridger <739272884@qq.com>
2020-06-24 15:17:41 +0800

    feat: 科利华

---
+++ js/科利华map.js
@@ -0,0 +1,29 @@
+let isHTMLTag = makeMap(
+    'html,body,base,head,link,meta,style,title,' +
+    'address,article,aside,footer,header,h1,h2,h3,h4,h5,h6,hgroup,nav,section,' +
+    'div,dd,dl,dt,figcaption,figure,picture,hr,img,li,main,ol,p,pre,ul,' +
+    'a,b,abbr,bdi,bdo,br,cite,code,data,dfn,em,i,kbd,mark,q,rp,rt,rtc,ruby,' +
+    's,samp,small,span,strong,sub,sup,time,u,var,wbr,area,audio,map,track,video,' +
+    'embed,object,param,source,canvas,script,noscript,del,ins,' +
+    'caption,col,colgroup,table,thead,tbody,td,th,tr,' +
+    'button,datalist,fieldset,form,input,label,legend,meter,optgroup,option,' +
+    'output,progress,select,textarea,' +
+    'details,dialog,menu,menuitem,summary,' +
+    'content,element,shadow,template,blockquote,iframe,tfoot'
+  );
+
+  /**
+   * Make a map and return a function for checking if a key
+   * is in that map.
+   */
+  function makeMap (str) {
+
+    var map = Object.create(null);
+
+    var list = str.split(',');
+    list.forEach(el => {
+        map[el] = true;
+    });
+    return (val) => map[val.toLowerCase()];
+  }
+  let c = isHTMLTag('params');
