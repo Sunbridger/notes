b7cb166f842e6cedf2e6931c1a3c48001b3b24df
Sunbridger <739272884@qq.com>
2020-07-27 10:04:45 +0800

    feat: cmdamd

--- js/ajaxtest.js
+++ js/ajaxtest.js
@@ -1,4 +1,5 @@
-const axios = require('axios');
+// const axios = require('axios');
+import axios from 'axios';
 axios.get('https://mock.souche-inc.com/mock/5f17f3a058207567647e473b/example/getinfo').then((res) => {
     console.log(res.data);
 })
---
+++ js/cmdamd.js
@@ -0,0 +1,8 @@
+// typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+// define.cmd
+//   typeof define === 'function' && define.amd ? define(factory) :
+//   (global = global || self, global.Swiper = factory());
+
+
+console.log(typeof exports, ':');
+console.log(typeof define);
