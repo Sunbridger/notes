8139d19abd3d48e8ddb30e44e230a680d4cf72cb
Sunbridger <739272884@qq.com>
2020-07-30 16:20:33 +0800

    feat: 测试

--- js/@babel-core.js
+++ js/@babel-core.js
@@ -2,10 +2,8 @@ let babel = require('@babel/core');
 const code = 'let a = 4;';
 
 const opt = {};
-
 babel.transform(code, (err, result) => {
     console.log(result);
 });
-
 let babelP = require('@babel/parser');
 console.log(babelP);
