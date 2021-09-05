3c8f9d076fa7c694ef36e99f1476598267b043ff
七彩 <wangriqiao@cai-inc.com>
2021-09-05 11:28:47 +0800

    feat: type module

--- js/wechaty.js
+++ js/wechaty.js
@@ -1,7 +1,12 @@
-const { Wechaty } = require('wechaty');
+import { Wechaty } from 'wechaty'
 
-Wechaty.instance()
-    .on('scan', (url, code) => console.log(`Scan QR Code to login: ${code}\n${url}`))
-    .on('login', user => console.log(`User ${user} logined`))
-    .on('message', message => console.log(`Message: ${message}`))
-    .start()
+const bot = new Wechaty({
+  // 微信机器人使用了 iPad 协议登录，puppet 为 `PuppetPadplus`
+  puppet: new PuppetPadplus(),
+  name: 'daxiange'
+})
+
+bot
+  .on('scan', (qrcode, status) => console.log(`Scan QR Code to login: ${status}\nhttps://api.qrserver.com/v1/create-qr-code/?data=${encodeURIComponent(qrcode)}`))
+  .on('message',       message => console.log(`Message: ${message}`))
+  .start()
