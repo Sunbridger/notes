29c8270f99756040ee3ee56d040551a3a5513a80
七彩 <wangriqiao@cai-inc.com>
2021-09-30 11:44:51 +0800

    feat: remove

--- sentry-client-example/test.ts
+++
@@ -1,4 +0,0 @@
-const NodeSentryClientSDK = require('@zcy/node-sentry-client');
-let nodeSentry = new NodeSentryClientSDK('https://67b2e0a83e26429abfb760b424a8110c@front-sentry.zcygov.cn/213'); // 需要向牧羊申请新项目的 dns
-
-// nodeSentry.
